a9b8c2c52ad6365443a7d7d828747382f4209217
AshieCO <alexanderashie@gmail.com>
2019-02-17 20:27:27 -0500

    Add files via upload

--- clump/js/main.js
+++ clump/js/main.js
@@ -1,4 +1,6 @@
 var cardArray = [];
+var numberOfRows = 0;
+var cardCounter = 0;
 
 function CardObject (cardObjectId, image, title, description, url, source, loveValue, hateValue){
     this.cardObjectId = cardObjectId;
@@ -26,9 +28,15 @@ $.ajax({
             cardObjectId++;
         });
 
-        displayResults();
+        console.log(cardArray);
 
+        numberOfRows = cardArray.length / 6;
 
+        populateContainer(numberOfRows);
+
+        renderRows(numberOfRows);
+
+        //displayResults();
         
     
 
@@ -39,6 +47,7 @@ $.ajax({
 //console.log(cardArray[0].image);
 
 
+/*
 function displayResults() {
 
     var myDiv = document.getElementById("test");
@@ -58,9 +67,92 @@ function displayResults() {
 
     
 }
+*/
 
+function populateContainer(numberOfRows) {
 
+    var rowContainer = document.getElementById("container");
 
+    for (i = 0; i < numberOfRows; i++) {
+        var newRow = document.createElement("DIV");
+        newRow.className = "row";
+        newRow.id = "row" + i;
+
+        rowContainer.appendChild(newRow);
+    }
+}
+
+function renderRows(numberOfRows) {
+
+    for (i = 0; i < numberOfRows; i++) {
+
+        for (j = 0; j < 6; j++) {
+
+            renderCard(i, cardCounter);
+
+            cardCounter += 1;
+
+        }
+
+        cardCounter += 1;
+
+    }
+}
+
+function renderCard(i, cardCounter) {
+
+    var currentRow = document.getElementById("row" + i);
+
+    var newCol = document.createElement("DIV");
+    newCol.className = "col";
+
+    var newCard = document.createElement("DIV");
+    newCard.className = "card";
+    newCol.appendChild(newCard);
+
+    var newImage = document.createElement("IMG");
+    newImage.className = "card-img-top";
+    newImage.src = cardArray[cardCounter].image;
+    newCard.appendChild(newImage);
+
+    var newTitle = document.createElement("H5");
+    newTitle.className = "card-title";
+    newTitle.innerHTML = cardArray[cardCounter].title;
+
+    var newDescription = document.createElement("P");
+    newDescription.innerHTML = cardArray[cardCounter].description;
+
+    var newCardBody = document.createElement("DIV");
+    newCardBody.className = "card-body";
+    newCardBody.appendChild(newTitle);
+    newCardBody.appendChild(newDescription);
+    newCard.appendChild(newCardBody);
+
+    var newDivider = document.createElement("DIV");
+    newDivider.className = "divider";
+    newCardBody.appendChild(newDivider);
+
+    var newSourceButton = document.createElement("BUTTON");
+    newSourceButton.type = "button";
+    newSourceButton.className = "btn btn-outline-success btn-block";
+    newSourceButton.innerHTML = "Source";
+    newCardBody.appendChild(newSourceButton);
+
+    var newLoveButton = document.createElement("BUTTON");
+    newLoveButton.type = "button";
+    newLoveButton.className = "btn btn-outline-primary";
+    newLoveButton.innerHTML = "Love 0";
+    newCardBody.appendChild(newLoveButton);
+
+    var newHateButton = document.createElement("BUTTON");
+    newHateButton.type = "button";
+    newHateButton.className = "btn btn-outline-danger";
+    newHateButton.innerHTML = "0 Hate";
+    newCardBody.appendChild(newHateButton);
+
+    currentRow.appendChild(newCol);
+
+}
 
 
 
@@ -77,11 +169,6 @@ function incrementLove() {
 function incrementHate() {
     hateCounter += 1;
     return hateCounter + " Hate";
-}
-
-function docWriteTest() {
-
-    return;
 }
 
 function cardWriteTest() {
